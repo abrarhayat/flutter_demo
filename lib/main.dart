46a70a73736f59b7d0cc9ba3642a2d9a1708e1ef
Abrar Hayat <abrarhayat@gmail.com>
2021-08-09 01:51:41 +0600

    Added a scaffold, a body to that and a column widget with Elavated buttons, also added some bottom nav text

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,24 @@ void main() => runApp(AbrarApp()); //shorthand syntax
 class AbrarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // List<String> questions = ['Q1', 'Q2'];
+    var questions = ['Q1', 'Q2']; //type inferencing works by deafult
     return MaterialApp(
-      home: Text("Abrar's Demo App"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Test Appbar Title",
+          ),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: null, child: Text("TestQ1")),
+            ElevatedButton(onPressed: null, child: Text("TestQ2")),
+            ElevatedButton(onPressed: null, child: Text("TestQ3")),
+          ],
+        ),
+        bottomNavigationBar: Text("Bottom Nav Bar Text"),
+      ),
     );
   }
 }
