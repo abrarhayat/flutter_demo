9317bebac0cfff0915b284e76ec0e7b11110dce5
Abrar Hayat <abrarhayat@gmail.com>
2021-08-10 17:45:16 +0600

    Added demo functions into the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import "package:flutter/material.dart";
 void main() => runApp(AbrarApp()); //shorthand syntax
 
 class AbrarApp extends StatelessWidget {
+  void submitAnswer() {
+    print("Answer Submitted!");
+  }
+
   @override
   Widget build(BuildContext context) {
     // List<String> questions = ['Q1', 'Q2'];
@@ -20,9 +24,17 @@ class AbrarApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            ElevatedButton(onPressed: null, child: Text("TestQ1")),
-            ElevatedButton(onPressed: null, child: Text("TestQ2")),
-            ElevatedButton(onPressed: null, child: Text("TestQ3")),
+            ElevatedButton(
+                onPressed: submitAnswer, child: Text("TestQ1")), //named func
+            ElevatedButton(
+                onPressed: () => print("Answer 2 chosen!"), //anonymous func 1
+                child: Text("TestQ2")),
+            ElevatedButton(
+                onPressed: () { //anonymous func 2
+                  // ...
+                  print("Answer 3chosen!");
+                },
+                child: Text("TestQ3")),
           ],
         ),
         bottomNavigationBar: Text("Bottom Nav Bar Text"),
