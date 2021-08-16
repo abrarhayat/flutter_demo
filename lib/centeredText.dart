0275b10cd121bd5bb323879f4a8ba3bf7fc1cd8f
Abrar Hayat <abrarhayat@gmail.com>
2021-08-17 01:08:54 +0600

    Added a getter to get phrase based on score, also added the option to bold centered text widgets

--- lib/centeredText.dart
+++ lib/centeredText.dart
@@ -3,18 +3,25 @@ import "package:flutter/material.dart";
 class CenteredText extends StatelessWidget {
   final double fontSize;
   final String text;
-  CenteredText(this.fontSize, this.text);
+  final bool bold;
+  CenteredText({required this.fontSize, required this.text, this.bold = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(10),
-      child: Text(
-        text,
-        style: TextStyle(fontSize: fontSize),
-        textAlign: TextAlign.center,
-      ),
-    );
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: bold
+            ? Text(
+                text,
+                style: TextStyle(fontSize: fontSize),
+                textAlign: TextAlign.center,
+              )
+            : Text(
+                text,
+                style:
+                    TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -3,13 +3,25 @@ import "./centeredText.dart";
 
 class Result extends StatelessWidget {
   final int score;
+
   Result(this.score);
+
+  String get resultPhrase {
+    if (score >= 27) {
+      return "You are on the right path.";
+    } else if (score > 20) {
+      return "You need to work a little bit harder.";
+    }
+    return "You need to work much harder.";
+  }
+
   @override
   Widget build(BuildContext context) {
     print(score);
-    return Column(children: [
-      CenteredText(28, "Thank you for your Feedback"),
-      CenteredText(30, "Your Score: " + score.toString())
-    ]);
+    return Center(
+        child: CenteredText(
+            fontSize: 30,
+            text: "Your Score: " + score.toString() + "\n" + resultPhrase,
+            bold: true));
   }
 }
